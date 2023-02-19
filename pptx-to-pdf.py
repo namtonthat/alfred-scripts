65b1014197c328cfaba75ca7a20295fdfeca180d
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-02-19 11:26:51 +1100

    Add cloudwatch log and fix up pptx-to-pdf

--- pptx-to-pdf.py
+++ pptx-to-pdf.py
@@ -11,13 +11,18 @@ source_folder = "/Users/namtonthat/Library/CloudStorage/GoogleDrive-n.nam.tontha
 # changing directory to source
 os.chdir(source_folder)
 
-file_name = "{query}"
-print(file_name)
+file_name = sys.argv[1]
+# file_name = "L15"
+
 # assign and running the command of converting files through LibreOffice
-# command = f"{path_to_office}   --headless --convert-to pdf --outdir . {file_name}.pptx"
-# subprocess.run(command)
+command = f"{path_to_office}  --headless --convert-to pdf --outdir . {file_name}.pptx"
+subprocess.run(command, shell=True)
 
 # move files after completed
-# mv_command = f"mv {file_name} {source_folder}/Conversational\ Chinese/"
+mv_pdf_command = f"mv {file_name}.pdf \"{source_folder}/Conversational Chinese\""
+mv_pptx_command = f"mv {file_name}.pptx \"{source_folder}/Conversational Chinese/\""
+
+subprocess.run(mv_pdf_command, shell=True)
+subprocess.run(mv_pptx_command, shell=True)
 
 print('Converted')
