7c58808d1f02df01b1b3db9a5919f9d5e9e4c428
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-12-29 08:56:08 +1100

    remove fuel-finder

--- fuel-price-finder/install.sh
+++
@@ -1,8 +0,0 @@
-# Install dependencies and run the script
-export PYTHON_VERSION=3.10.9
-pyenv install $PYTHON_VERSION
-pyenv local alfred-scripts-$PYTHON_VERSION
-
-# Shell
-poetry shell
-poetry install --no-root
